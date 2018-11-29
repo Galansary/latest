a8dafa9271140d87726acb767883660e91b4af72
Gaafar Alansary <bara6a@hotmail.com>
2018-11-29 20:49:14 +0000

    chromefix

--- SelenuimTest/UnitTest1.cs
+++ SelenuimTest/UnitTest1.cs
@@ -1,35 +1,88 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
 
-namespace WebDriverExtensions
+namespace SeleniumBingTests
 {
-    public static class WebElementExtensions
+    /// <summary>
+    /// Summary description for MySeleniumTests
+    /// </summary>
+    [TestClass]
+    public class MySeleniumTests
     {
-        public static bool ElementIsPresent(this IWebDriver driver, By by)
+        private TestContext testContextInstance;
+        private IWebDriver driver;
+        private string appURL;
+
+
+        public MySeleniumTests()
         {
-            try
+        }
+
+        [TestMethod]
+        [TestCategory("Chrome")]
+        public void TheChromeTest()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            Assert.AreEqual(true ,driver.FindElement(By.XPath("/html/body/div/h4")).Displayed); // Visible Works headers BPcalculator
+            Assert.AreEqual(true ,driver.FindElement(By.XPath("/html/body/nav/div/div[1]/a")).Displayed); // Visible Works headers in Black
+        }
+        [TestMethod]
+        [TestCategory("fireFox")]
+        public void theFireFoxTest()
+        {
+            driver.Navigate().GoToUrl(appURL + "/");
+            Assert.AreEqual(true, driver.FindElement(By.XPath("/html/body/div/h4")).Displayed); // Visible Works headers BPcalculator
+            Assert.AreEqual(true, driver.FindElement(By.XPath("/html/body/nav/div/div[1]/a")).Displayed); // Visible Works headers in Black
+        }
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
             {
-                return driver.FindElement(by).Displayed;
+                return testContextInstance;
             }
-            catch (NoSuchElementException)
+            set
             {
-                return false;
+                testContextInstance = value;
             }
         }
-    }
 
-    [TestClass]
-    public class ExtensionUnitTests
-    {
-        [TestMethod]
-        public void FindVisibleElements()
+        [TestInitialize()]
+        public void SetupTest()
         {
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://webapptest-bpcalc.azurewebsites.net/");
-            Assert.AreEqual(true, driver.ElementIsPresent(By.XPath("/html/body/div/h4"))); // Visible Works
-            Assert.AreEqual(true, driver.ElementIsPresent(By.XPath("/html/body/nav/div/div[1]/a"))); // Visible Works
-          
+            appURL = "http://webapptest-bpcalc.azurewebsites.net/";
+
+            string browser = "Chrome";
+            switch (browser)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "IE":
+                    driver = new InternetExplorerDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+                    break;
+            }
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
             driver.Quit();
         }
     }
